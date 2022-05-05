5a46be1c17670a87b3c25d27277a4fccfcbff2f2
NATALIA KAHORI KANESHIMA <natalia.kaneshima@edu.unifil.br>
2022-05-05 16:27:58 -0300

    teste funionando por enquanto

---
+++ exercicio_teste/exercicio_teste/src/main/java/br/com/kaneshima/exercicio_teste/ExercicioTesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.kaneshima.exercicio_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercicioTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercicioTesteApplication.class, args);
+	}
+
+}
---
+++ exercicio_teste/exercicio_teste/src/test/java/br/com/kaneshima/exercicio_teste/ExercicioTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kaneshima.exercicio_teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
