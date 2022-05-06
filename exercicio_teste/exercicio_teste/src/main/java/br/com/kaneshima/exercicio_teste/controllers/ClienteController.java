b8a2adff3ded12d9908e5a27ed941a0eb405ba44
NATALIA KAHORI KANESHIMA <natalia.kaneshima@edu.unifil.br>
2022-05-06 15:36:20 -0300

    esquecido

--- exercicio_teste/exercicio_teste/src/main/java/br/com/kaneshima/exercicio_teste/controllers/ClienteController.java
+++ exercicio_teste/exercicio_teste/src/main/java/br/com/kaneshima/exercicio_teste/controllers/ClienteController.java
@@ -1,14 +1,16 @@
 package br.com.kaneshima.exercicio_teste.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.kaneshima.exercicio_teste.models.Cliente;
 
 @RestController
+@RequestMapping(path = "/cliente")
 public class ClienteController {
 	
-	@GetMapping(path = "cliente/qualquer")
+	@GetMapping(path = "/qualquer")
 	public Cliente obterCliente() {
 		return new Cliente(28, "Pedro", "123.456.789-00");
 	}
